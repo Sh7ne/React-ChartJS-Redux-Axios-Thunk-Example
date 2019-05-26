3fa9e1fe158545917ff939495070e54da80ef4af
Yao Zhao <Shinenirvana@googlemail.com>
2019-05-26 04:36:12 -0400

    async Didmount

--- src/containers/radarContainer.js
+++ src/containers/radarContainer.js
@@ -4,12 +4,29 @@ import * as actionCreators from "../actions/RadarAction"
 import RadarDiagram from '../components/Radar.js';
 
 class RadarContainer extends React.Component {
-    componentDidMount() {
-        this.props.loadReviewData();
+    constructor(props){
+        super(props)
+        console.log(props)
+        this.state = {
+            loaded: false
+          }
+    }
+
+    async componentDidMount() {
+        await this.props.loadReviewData();
+        this.setState({loaded: true})
         console.log(this.props.data);
     }
+    
+    content() {
+        return (<RadarDiagram data={this.props.data}></RadarDiagram>
+        )
+    }
+
     render() {
-        return <RadarDiagram data={this.props.data}></RadarDiagram>
+        return (<div>
+            {this.state.loaded ? this.content() : <div>Still Loading...</div>}
+        </div>)
     }
 }
 
